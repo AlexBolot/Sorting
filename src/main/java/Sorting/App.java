d787c1c51d27272b3778dbad9a2711df34c0a87c
alexandre <bolotalex06@gmail.com>
2017-03-14 09:59:12 +0100

    We now have MaxLin, TriSelection, TriFusion

--- src/main/java/Sorting/App.java
+++ src/main/java/Sorting/App.java
@@ -1,5 +1,7 @@
 package Sorting;
 
+import java.util.Random;
+
 /**
  Hello world!
  */
@@ -7,6 +9,28 @@ public class App
 {
     public static void main (String[] args)
     {
-        System.out.println("Hello World!");
+        Random random = new Random();
+    
+        for (int i = 1000; i <= 1000000; i *= 10)
+        {
+            int[] tab = new int[i];
+        
+            for (int j = 0; j <= tab.length - 1; j++)
+            {
+                tab[j] = random.nextInt();
+            }
+        
+            long oldt = System.nanoTime();
+            Sorting.MaxLin(tab);
+            System.out.println(i + "\tMaxLin :\t\t" + (System.nanoTime() - oldt));
+        
+            oldt = System.nanoTime();
+            Sorting.MaxTriSelection(tab);
+            System.out.println(i + "\tMaxSelection :\t" + (System.nanoTime() - oldt));
+        
+            oldt = System.nanoTime();
+            Sorting.MaxTriFusion(tab);
+            System.out.println(i + "\tMaxFusion :\t\t" + (System.nanoTime() - oldt) + "\n");
+        }
     }
 }
---
+++ src/main/java/Sorting/Sorting.java
@@ -0,0 +1,86 @@
+package Sorting;
+
+@SuppressWarnings("ALL")
+public class Sorting
+{
+    public static int MaxLin (int[] table)
+    {
+        int maxValue = 0;
+        
+        for (int i = 0; i < table.length; i++)
+        {
+            if(maxValue < table[i]) maxValue = table[i];
+        }
+        
+        return maxValue;
+    }
+    
+    public static int MaxTriSelection (int[] table)
+    {
+        return TriSelection(table)[table.length - 1];
+    }
+    
+    public static int[] TriSelection (int[] table)
+    {
+        int minValue = 0;
+        int i = 0;
+        int j = 0;
+        
+        for (i = 0; i < table.length - 1; i++)
+        {
+            minValue = i;
+            
+            for (j = i; j < table.length; j++)
+            {
+                if(table[minValue] > table[j]) minValue = j;
+            }
+            
+            int temp = table[i];
+            table[i] = table[minValue];
+            table[minValue] = temp;
+        }
+        
+        return table;
+    }
+    
+    public static int MaxTriFusion (int[] table)
+    {
+        if(table.length > 1)
+        {
+            return TriFusion(table, 0, table.length - 1)[table.length - 1];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    
+    public static int[] TriFusion (int[] table, int from, int to)
+    {
+        if(from != to)
+        {
+            int[] tab1 = TriFusion(table, from, (from + to) / 2);
+            int[] tab2 = TriFusion(table, (from + to) / 2 + 1, to);
+            return Fusion(tab1, tab2);
+        }
+        
+        return new int[]{table[0]};
+    }
+    
+    public static int[] Fusion (int[] table1, int[] table2)
+    {
+        int[] newTab = new int[table1.length + table2.length];
+        
+        for (int i = 0; i < table1.length - 1; i++)
+        {
+            newTab[i] = table1[i];
+        }
+        
+        for (int i = 0; i < table2.length - 1; i++)
+        {
+            newTab[table1.length + i] = table2[i];
+        }
+        
+        return TriSelection(newTab);
+    }
+}
